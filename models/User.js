89baefe8f41fab200a59622fa8967333a95c8d9d
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-27 21:46:35 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: String,
-  password: String,
-  isPremium: { type: Boolean, default: false }
+  password: String
 });
 
 module.exports = mongoose.model("User", UserSchema);
