732aeeb450ec2ed65ddd7855f5cad0282268d3e7
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-27 22:08:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,3 +24,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("Server running"));
+const transactionRoutes = require("./routes/transaction");
+
+app.use("/api/transactions", transactionRoutes);
